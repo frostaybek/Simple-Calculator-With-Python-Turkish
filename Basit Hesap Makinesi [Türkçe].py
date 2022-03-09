d6cafeef76815328c49eeceae66b7377f2e64057
Ayaz <frostaybek@gmail.com>
2022-03-09 15:52:38 +0300

    Updated
    9.3.2022 Updated

--- Basit Hesap Makinesi [Türkçe].py
+++ Basit Hesap Makinesi [Türkçe].py
@@ -1,46 +1,55 @@
 def menu():
-	print("""Menu
----------
+	print("""Ayaz Aybek yapımı
+	
+-----------
 1-Toplama
 2-Çıkarma
 3-Çarpma
 4-Bölme
-5-Çıkış""")
+5-Çıkış
+-----------
+""")
 
 def yapilansecim(secim):
 	if secim == 1:
-		print("Toplama seçildi")
+		print("""
+TOPLAMA SEÇİLDİ
+""")
 		x=input("Birinci Sayı : ")
 		y=input("İkinci Sayı : ")
 		toplam=int(x)+int(y)
 		print(toplam)
 		
-
 	elif secim == 2:
-		print("çıkarma seçildi")
+		print("""
+ÇIKARMA SEÇİLDİ
+""")
 		x=input("Birinci Sayı : ")
 		y=input("İkinci Sayı : ")
 		çıkar=int(x)-int(y)
 		print(çıkar)
 
 	elif secim == 3:
-		print("Çarpma seçildi")
+		print("""
+ÇARPMA SEÇİLDİ
+""")
 		x=input("Birinci Sayı : ")
 		y=input("İkinci Sayı : ")
 		carp=int(x)*int(y)
 		print(carp)
 
 	elif secim == 4:
-		print("bölme seçildi")
+		print("""
+BÖLME SEÇİLDİ
+""")
 		x=input("Birinci Sayı : ")
 		y=input("İkinci Sayı : ")
 		bol=int(x)/int(y)
 		print(bol)
 		
 	elif secim == 5:
-	    print("Çıkış Seçildi")
+	    print("ÇIKIŞ SEÇİLDİ")
 	    pass
-	    
 	    
 secim=0
 
@@ -48,19 +57,3 @@ while int(secim) != 5:
     menu()
     secim=input("Seçiminizi yapınız : ")
     yapilansecim(int(secim))
-    
-	    
-
-
-
-
-
-
-
-
-    
-
-
-    
-
-
